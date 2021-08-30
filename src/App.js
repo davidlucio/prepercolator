ac69afd5ab8e9e80d34d8b526a0ecd6d77d4344f
David Lucio <david.a.lucio@gmail.com>
2021-08-29 23:15:40 -0700

    Added new SASS implementation and different file format for assets. Run npm install to get the formatting. DO NOT OVERWRITE NEW package.json SCRIPTS!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content'
+
+export default function App() {
+    return (
+        <>
+
+            <Header />
+            
+            <Content />
+
+            <Footer />
+
+        </>
+    );
+}
