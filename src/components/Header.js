7305819ff7c71fcf4d50d7115201dc66a446f781
David Lucio <david.a.lucio@gmail.com>
2021-09-02 17:51:27 -0700

    Sending upward in case I fuck up

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,41 +4,44 @@ import { Link, useHistory } from "react-router-dom";
 import Logo from "../assets/images/logo_bold.png";
 
 export default function Header({ user }) {
-  let history = useHistory();
-  const logout = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    history.push("/");
-    window.location.reload();
-  };
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-            <img className="logo" src={Logo} alt="Prepercolator Logo"></img>
-          </li>
-          <li>
-            <Link to="/user/profile">Profile</Link>
-          </li>
-          {user.is_admin === true ? (
-            <li>
-              <Link to="/supersecretbusinessportal">Business</Link>
-            </li>
-          ) : null}
-          {user ? (
-            <li>
-              <button onClick={logout}>Logout</button>
-            </li>
-          ) : null}
-          {user.newOrder ? (
-            <li>
-              <Link to="/order">Your Current Order</Link>
-            </li>
-          ) : null}
-        </ul>
-      </nav>
-    </header>
-  );
+    let history = useHistory();
+    const logout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        history.push("/");
+        window.location.reload();
+    };
+    return (
+        <header>
+            <Link to="/">
+                <img className="logo" src={Logo} alt="Prepercolator Logo" />
+            </Link>
+            
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/user/profile">Profile</Link>
+                    </li>
+                    {user.is_admin === true ? (
+                        <li>
+                            <Link to="/supersecretbusinessportal">Business</Link>
+                        </li>
+                    ) : null}
+                    {user ? (
+                        <li>
+                            <button onClick={logout}>Logout</button>
+                        </li>
+                    ) : null}
+                    {user.newOrder ? (
+                        <li>
+                            <Link to="/order">Your Current Order</Link>
+                        </li>
+                    ) : null}
+                </ul>
+            </nav>
+        </header>
+    );
 }
